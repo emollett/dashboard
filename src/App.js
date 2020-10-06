6a2431b0cbc6ac97d3e4f349ab8095b47a57463f
Eleanor Mollett <eleanor.mollett@gmail.com>
2020-10-06 16:14:48 +0100

    Added styling

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import { renderButton, checkSignedIn } from "./utils";
 import Report from './Report.jsx';
+import styled from 'styled-components'
 
 function App() {
     const [isSignedIn, setIsSignedIn] = useState(false);
@@ -27,19 +28,33 @@ function App() {
         window.gapi.load("auth2", init);
     });
 
+    const Tiles = styled.div`
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: flex-start;
+        margin: 1em;
+    `;
+
     return (
         <div className="App">
             <h1>Stakeholders team's dashboard</h1>
             {!isSignedIn ? (
                 <div id="signin-button"></div>
             ) : (
-                <>
-                   <Report siteName={"Comment collection"} viewID={process.env.REACT_APP_VIEW_ID_1}/>
-                   <Report siteName={"UKPS"} viewID={process.env.REACT_APP_VIEW_ID_2}/>
-               </>
+                <Tiles>
+                    <Report siteName={"Comment collection"} viewID={process.env.REACT_APP_VIEW_ID_1}/>
+                    <Report siteName={"UKPS"} viewID={process.env.REACT_APP_VIEW_ID_2}/>
+                    <Report siteName={"META"} viewID={process.env.REACT_APP_VIEW_ID_1}/>
+                    <Report siteName={"Healthtech Connect"} viewID={process.env.REACT_APP_VIEW_ID_2}/>
+                    <Report siteName={"NICE Docs"} viewID={process.env.REACT_APP_VIEW_ID_1}/>
+                    <Report siteName={"NICE Accounts - new"} viewID={process.env.REACT_APP_VIEW_ID_2}/>
+                    <Report siteName={"NICE Accounts - old"} viewID={process.env.REACT_APP_VIEW_ID_1}/>
+               </Tiles>
             )}
         </div>
     );
 }
 
 export default App;
+
+
--- src/Report.jsx
+++ src/Report.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { queryReport } from "./queryReport";
+import styled from 'styled-components'
 
 const Report = (props) => {
     const [data, setData] = useState([]);
@@ -28,14 +29,22 @@ const Report = (props) => {
             .catch((error) => console.error(error));
     }, []);
 
+    const Tile = styled.div`
+        background-color: #fafafa;
+        flex: 0 1 25em;
+        margin: 1em;
+        box-shadow: 0.1em 0.1em 0.3em #e9e9e9;
+        padding-bottom: 0.5em;
+    `;
+
     return (
-        <div>
+        <Tile>
             <h2>{props.siteName}</h2>
             <p>Total - {total} visits</p>
             {data.map((row) => (
-            <div key={row.date}>{`${row.date}: ${row.visits} visits`}</div>
-            ))}
-        </div>
+                <div key={row.date}>{`${row.date}: ${row.visits} visits`}</div>
+                ))}
+        </Tile>
     )
 };
 
